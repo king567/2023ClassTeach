157ae0aabc84d95fedfa8607bc02cfff046195bb
king567 <king0922go@gmail.com>
2023-07-27 13:30:59 +0800

    新增期中專案
    新增期中專案
    創建紀錄用資料夾

---
+++ MidProject/StudentScores/Scores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentScores
+{
+    public class Scores
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Chinese { get; set; }
+
+        public int English { get; set; }
+
+        public int Math { get; set; }
+    }
+}
